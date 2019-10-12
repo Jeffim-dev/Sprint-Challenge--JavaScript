70d0eb6276cc3b8e78d6256618d6d8c2e367fe24
Jeff-Im <everest.k6@gmail.com>
2019-10-12 11:36:01 -0700

    Initial commit

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a,b, cb) {
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,18 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+function greeting(a, b) {
+  return `Hello ${a} ${b}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +37,10 @@
 
 // Explanation: 
 
+/*
+// nestedfunction is looking for a variable 'internal' which is not the same scope. 
+   It looks up the outer scope of the fucntion until it finds the variable. 
+*/   
 
 const external = "I'm outside the function";
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,59 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const dinosaursTy = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous",   
+  // roar: function() {
+  //   return "RAWERSRARARWERSARARARRRR!"
+  // }
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const dinosaursSt = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"    
+ }
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const dinosaursVe = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"    
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaursTy.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaursVe.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaursSt.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaursTy.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+dinosaursSt.roar = function() {
+  return "RAWERSRARARWERSARARARRRR!"
+}
+
+console.log(dinosaursSt.roar());
 
 
 // ==== Arrays ====
@@ -51,7 +83,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+graduates.forEach(list => universities.push(`${list.university}`));
+universities.sort();
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -61,10 +97,18 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(info => contactInfo.push(`${info.first_name} ${info.email}`));
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for(let i = 0; i < graduates.length; i++) {
+  if(graduates[i].university.includes("Uni")) {
+    unisWithUni.push(graduates[i].university);
+  }
+}
+
 console.log(unisWithUni);
 
 
@@ -90,7 +134,10 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const displayNames = [];
+zooAnimals.forEach(name => displayNames.push(`Name: ${name.animal_name}, Scientific: ${name.scientific_name}`));
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -99,7 +146,8 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(name => name.animal_name.toLowerCase());
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -107,7 +155,9 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+
+const lowPopulationAnimals = zooAnimals.filter(size => size.population < 5);
+
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -115,7 +165,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+
+populationTotal = zooAnimals.reduce((animal, total) => animal += total.population, 0);
+
 console.log(populationTotal);
 
 
